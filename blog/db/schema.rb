df1ead98948ededa8ef8ae09128a5ab337975b07
ahmad-diaa <ahmaddiaa93@gmail.com>
2015-02-18 21:44:52 +0200

    added category & working on listing by category

--- blog/db/schema.rb
+++ blog/db/schema.rb
@@ -11,11 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150216131054) do
+ActiveRecord::Schema.define(version: 20150218192638) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "text"
+    t.integer  "category_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
