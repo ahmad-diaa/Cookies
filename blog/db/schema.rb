eec8d190e745028bc198964494f4354515c6f21e
maggie-moheb <maggiemoheb@Maggies-MacBook-Pro.local>
2015-02-19 15:08:02 +0200

    newest updates in blog

--- blog/db/schema.rb
+++ blog/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150218192638) do
+ActiveRecord::Schema.define(version: 20150218200530) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20150218192638) do
     t.integer  "category_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "category"
   end
 
   create_table "categories", force: :cascade do |t|
