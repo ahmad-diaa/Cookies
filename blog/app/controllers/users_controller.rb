03122cbfb264d3d8ce650f90e14b703d989df590
Daliamaarek <daliamaarek@gmail.com>
2015-02-21 21:24:22 +0200

    Designed the website, added a signature link

--- blog/app/controllers/users_controller.rb
+++ blog/app/controllers/users_controller.rb
@@ -1,10 +1,13 @@
 class UsersController < ApplicationController
+
   def new
   @user = User.new
-   end
+     end
+ 
  def show
  	 @user = User.find(params[:id])
- end
+  end
+
 def create
     @user = User.new(user_params)
     if @user.save
@@ -15,9 +18,18 @@ def create
       render 'new'
     end
 end
-    private
+  
 
+  def edit
+ @user = User.find(params[:id])
+      @user.signature = params[:text1]
+      @user.save
+  end
+
+  private
     def user_params
       params.require(:user).permit(:name, :email, :password, :password)
   end
 end
+
+
