49d8d475a7a40f1df478284b25fb252aefff3ffc
ahmad3m3 <ahmad.abdaraheeem@gmail.com>
2015-02-23 19:00:21 +0200

    Image uploader is now working

--- blog/app/models/carrierwave_image.rb
+++ blog/app/models/carrierwave_image.rb
@@ -1,10 +1,14 @@
+# class CarrierwaveImage < ActiveRecord::Base
+# 	  def change
+#     create_table :carrierwave_images do |t|
+#       t.string :asset ,:null => false
+# belongs_to :users
+#       t.timestamps null: false
+#        add_foreign_key :carrierwave_images, :users
+#    	 end
+# 	end
+# end
 class CarrierwaveImage < ActiveRecord::Base
-	  def change
-    create_table :carrierwave_images do |t|
-      t.string :asset ,:null => false, :default => '/images/no_user_profile-pic.jpg'
-belongs_to :users
-      t.timestamps null: false
-       add_foreign_key :carrierwave_images, :users
-   	 end
-	end
+mount_uploader  :asset, AssetUploader
+
 end
