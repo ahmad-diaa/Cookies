2b605b6a18371335ff7edfe438daa1b7d38812a5
maggie-moheb <maggiemoheb@Maggies-MacBook-Pro.local>
2015-02-25 00:06:30 +0200

    Adiing my changes to the blog

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -1,43 +1,50 @@
 class ArticlesController < ApplicationController
 	def index
+		@user = User.find(params[:user_id])
     	@articles = Article.order(id: :desc) 
     end
 
 	def show
+		@user = User.find(params[:user_id])
 		@article = Article.find(params[:id])
 	end
 
 	def new
+		@user = User.find(params[:user_id])
 		@article = Article.new
 	end
 
 	def edit
+		@user = User.find(params[:user_id])
 		@article = Article.find(params[:id])
 	end
 
 	def create
+		@user = User.find(params[:user_id])
 		@article = Article.new(article_params)
 
 		if @article.save
-			redirect_to articles_path
+			redirect_to user_articles_path
 		else
 			render 'new'
 		end
 	end
 	def update
+		@user = User.find(params[:user_id])
   		@article = Article.find(params[:id])
  	
   		if @article.update(article_params)
-    		redirect_to article_path
+    		redirect_to user_article_path
   		else
     		render 'edit'
   		end
 	end
 	def destroy
+		@user = User.find(params[:user_id])
   		@article = Article.find(params[:id])
   		@article.destroy
  
-  		redirect_to articles_path
+  		redirect_to user_articles_path
 	end
 	private 
 		def article_params
